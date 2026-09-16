1a8c331d31936af6080d91ea652a27a6fd721e22
Li <jili1@uiwtx.edu>
2026-09-16 15:43:30 -0500

    adding combo tags

--- Source/Capture/Private/Animation/AN_SendGameplayEvent.cpp
+++ Source/Capture/Private/Animation/AN_SendGameplayEvent.cpp
@@ -2,4 +2,23 @@
 
 
 #include "Animation/AN_SendGameplayEvent.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
+void UAN_SendGameplayEvent::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	if (!MeshComp->GetOwner())
+		return;
+
+	const UAbilitySystemComponent* OwnerASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(MeshComp->GetOwner());
+	if (OwnerASC)
+		return;
+
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(MeshComp->GetOwner(), EventTag, FGameplayEventData());
+}
+
+FString UAN_SendGameplayEvent::GetNotifyName_Implementation() const
+{
+	
+}
