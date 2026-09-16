2d7d40bde0c6c061414745cce7467db012cc8f17
Li <jili1@uiwtx.edu>
2026-09-16 15:53:45 -0500

    added send notify

--- Source/Capture/Private/Animation/AN_SendGameplayEvent.cpp
+++ Source/Capture/Private/Animation/AN_SendGameplayEvent.cpp
@@ -4,6 +4,7 @@
 #include "Animation/AN_SendGameplayEvent.h"
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemBlueprintLibrary.h"
+#include "GameplayTagsManager.h"
 
 void UAN_SendGameplayEvent::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
 {
@@ -20,5 +21,12 @@ void UAN_SendGameplayEvent::Notify(USkeletalMeshComponent* MeshComp, UAnimSequen
 
 FString UAN_SendGameplayEvent::GetNotifyName_Implementation() const
 {
-	
+	if (EventTag.IsValid())
+	{
+		TArray<FName> TagNames;
+		UGameplayTagsManager::Get().SplitGameplayTagFName(EventTag, TagNames);
+		return TagNames.Last().ToString();
+	}
+
+	return "None";
 }
