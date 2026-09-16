c30dfa73e4d1869ee71ed52613f3af4c488bac18
Li <jili1@uiwtx.edu>
2026-09-16 16:27:44 -0500

    added next combo record in the combo ability

--- Source/Capture/Private/Animation/AN_SendGameplayEvent.cpp
+++ Source/Capture/Private/Animation/AN_SendGameplayEvent.cpp
@@ -13,7 +13,7 @@ void UAN_SendGameplayEvent::Notify(USkeletalMeshComponent* MeshComp, UAnimSequen
 		return;
 
 	const UAbilitySystemComponent* OwnerASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(MeshComp->GetOwner());
-	if (OwnerASC)
+	if (!OwnerASC)
 		return;
 
 	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(MeshComp->GetOwner(), EventTag, FGameplayEventData());
